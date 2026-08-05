d00e713fd57592337b79311a83e573bd2cf843c3
Isaiah Lopez <isaiah.u.lopez@gmail.com>
2026-08-04 22:30:39 -0400

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int max = 0;
+        int n = heights.length - 1;
+
+        int l = 0;
+        int r = n;
+
+        while(l < r){
+
+            int curr_max = (r - l) * Math.min(heights[l], heights[r]);
+            max = Math.max(max, curr_max);
+
+            if(heights[l] < heights[r]){
+                l++;
+            }
+
+            else{
+                r--;
+            }
+        }
+        return max;
+    }
+}
+
