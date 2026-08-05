4c999bab9170c33a9209af1ab57d3f4e520b514f
Isaiah Lopez <isaiah.u.lopez@gmail.com>
2026-08-04 23:13:25 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int left = 0;
+        int right = 1;
+
+        while(right < prices.length){
+            if(prices[left] < prices[right]){
+                profit = Math.max(profit, prices[right] - prices[left]);
+            }
+            else{
+                left = right;
+            }
+            right++;
+        }
+
+        return profit;
+    }
+}
