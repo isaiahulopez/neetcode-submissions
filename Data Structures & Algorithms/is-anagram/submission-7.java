8de322fe02aee979f6ce6abf05628dca5c4cc667
Isaiah Lopez <isaiah.u.lopez@gmail.com>
2026-08-20 23:53:35 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] alph = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            alph[s.charAt(i) - 'a']++;
+            alph[t.charAt(i) - 'a']--;
+        }
+
+        for(int i: alph){
+            if(i != 0) return false;
+        }
+
+        return true;
+        
+    }
+}
