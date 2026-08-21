0ea76add3a86ef86f644917a418b70fb71dcf1f6
Isaiah Lopez <isaiah.u.lopez@gmail.com>
2026-08-21 00:32:39 -0400

    Add: valid-binary-search-tree - submission-8

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-8.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, null, null);
+    }
+    public boolean valid(TreeNode root, Integer min, Integer max){
+        if(root == null) return true;
+
+        if(min != null && min >= root.val) return false;
+        if(max != null && max <= root.val) return false;
+
+        return valid(root.right, root.val, max) && valid(root.left, min, root.val);
+    }
+}
